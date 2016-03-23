1549b0199f63c1dfde3a7d5bf54f9fa7dc051d02
meditans <meditans@gmail.com>
2016-03-23 17:58:01 +0100

    Added const and composition combinators

--- src/Common.hs
+++ src/Common.hs
@@ -1,9 +1,11 @@
 module Common where
 
 import Lib
-import Prelude hiding ((^))
+import Prelude hiding ((^), const)
 
 unit = make_var "unit"
+
+const = x ^ y ^ x
 
 couple = (a ^ b ^ x ^ (x # a # b))
 pi1 = (a ^ a # (x ^ y ^ x))
@@ -17,3 +19,6 @@ funPlus = (f ^ g ^ a ^ x ^ y ^ a # (a ^ x # (f # a)) # (a ^ y # (g # a)))
 
 split = (f ^ g ^ a ^ x ^ x # (f # a) # (g # a))
 funTimes = (f ^ g ^ a ^ a # (i ^ j ^ x ^ x # (f # i) # (g # j)))
+
+comp = f ^ g ^ x ^ f # (g # x)
+comp3 = f ^ g ^ h ^ x ^ f # (g # (h # x))
