1c4561b91664a65b940abf326dda501594db7d57
meditans <meditans@gmail.com>
2016-04-02 17:00:48 +0200

    Added comments on combinator functions

--- src/Common.hs
+++ src/Common.hs
@@ -14,10 +14,14 @@ pi2 = (a ^ a # (x ^ y ^ y))
 inl = (a ^ x ^ y ^ x # a)
 inr = (a ^ x ^ y ^ y # a)
 
+-- caseSplit viene di solito indicato con [...]
 caseSplit = (f ^ g ^ a ^ a # f # g)
+-- funPlus viene di solito indicato con f + g
 funPlus = (f ^ g ^ a ^ x ^ y ^ a # (a ^ x # (f # a)) # (a ^ y # (g # a)))
 
+-- split viene di solito indicato con <...>
 split = (f ^ g ^ a ^ x ^ x # (f # a) # (g # a))
+-- funTimes viene di solito indicato con f x g
 funTimes = (f ^ g ^ a ^ a # (i ^ j ^ x ^ x # (f # i) # (g # j)))
 
 comp = f ^ g ^ x ^ f # (g # x)
